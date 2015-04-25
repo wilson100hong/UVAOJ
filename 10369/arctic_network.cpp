b74ad8c5a7f9e405dfbbf02e42441a2625ecea12
Wilson Hong <wilsonhong@wilsonhong-macbookpro2.roam.corp.google.com>
2015-04-25 16:10:31 -0700

    10369 AC You need to use Kruskal.. Prim probably does not work

---
+++ 10369/arctic_network.cpp
@@ -0,0 +1,78 @@
+#include <algorithm> 
+#include <iostream>
+#include <iomanip>
+#include <map>
+#include <utility>
+#include <vector>
+#include <math.h> 
+
+using namespace std;
+
+struct Edge {
+  int u;
+  int v;
+  int cost;
+};
+
+bool operator<(const Edge& e1, const Edge& e2) {
+  return e1.cost < e2.cost;
+}
+
+// disjoint set
+int find(vector<int>& parents , int u) {
+  if (parents[u] != u) {
+    parents[u] = find(parents, parents[u]);
+  }
+  return parents[u];
+}
+
+void merge(vector<int>& parents, int u, int v) {
+  parents[find(parents, v)] = find(parents, u);
+}
+
+int main() {
+  // Read input
+  int T, S, P, X, Y;
+  cin >> T;
+  vector<pair<int, int>> outposts;
+  vector<Edge> edges;
+  vector<int> parents;
+  while (T-- > 0) {
+    cin >> S >> P;
+    outposts.clear();
+    parents.clear();
+    for (int p = 0; p < P; ++p) {
+      cin >> X >> Y;
+      outposts.push_back(make_pair(X, Y));
+      parents.push_back(p);  // disjoint set default parents
+    }
+
+    edges.clear();
+    for (int i = 0; i < outposts.size(); ++i) {
+      for (int j = i + 1; j < outposts.size(); ++j) {
+        int dx = outposts[j].first - outposts[i].first;
+        int dy = outposts[j].second - outposts[i].second;
+        int cost = dx * dx + dy * dy;
+        edges.push_back({i, j, cost});
+        edges.push_back({j, i, cost});
+      }
+    }
+    sort(edges.begin(), edges.end());
+
+    int max = 0;
+    // Kruskal
+    auto iter = edges.begin();
+    for (int i = 0; i < P - S  && iter != edges.end(); ++i) {
+      // if forms cycle (same set)
+      while (find(parents, iter->u) == find(parents, iter->v)) {
+        ++iter;
+      }
+
+      max = iter->cost;
+      merge(parents, iter->u, iter->v);
+      ++iter;
+    }
+    double ans = sqrt(max);
+    cout << fixed << setprecision(2) << ans << endl;
+  }
+}
