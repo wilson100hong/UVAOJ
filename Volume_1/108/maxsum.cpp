00fe449d6c700650d19f748eb2cbc0865cbda27d
Wilson <wilson@skydio.com>
2016-09-25 13:04:25 -0700

    commit somehting

--- Volume_1/108/maxsum.cpp
+++ Volume_1/108/maxsum.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
+
 using namespace std;
+
 int max1d(int ary[], int len) {
   int maxEndHere = ary[0];
   int maxSoFar = ary[0];
@@ -16,29 +18,20 @@ int max1d(int ary[], int len) {
 }
 
 int max2d(int ary[], int n) {
-  // Herr the len or ary will be n^2
+  // sum[k] is the summation of ary[i][k] + ... +  ary[j][k]
   int* sum = new int[n];
   int max = ary[0];
   for (int i = 0; i < n; ++i) {
-    // Reset sum here
+    // Reset sum
     for (int k = 0; k < n; ++k) {
       sum[k] = 0;
     }
 
     for (int j = i; j < n; ++j) {
-      // calculate sum[], which is from row i to j here  
       for (int k = 0; k < n; ++k) {
         sum[k] += ary[j * n + k];  
       }
       int maxsofar = max1d(sum, n);
-      /*
-      cout << "maxsofar " << i << "-" << j << ":" << maxsofar << endl;  
-      cout << "sum: ";
-      for (int k = 0; k < n; ++k) {
-        cout << " " <<  sum[k]; 
-      }
-      cout << endl;
-      */
       if (maxsofar > max) {
         max = maxsofar;  
       }
@@ -48,11 +41,6 @@ int max2d(int ary[], int n) {
 }
 
 int main() {
-  /*
-  int in[] = {9, 0, -13, 2};
-  cout << max1d(in, 4) << endl;
-  */
-  
   int n;
   cin >> n;
   int n2 = n * n;
