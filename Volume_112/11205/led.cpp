4b2239844246404ab6b78bc0a85d0cee5b86e2e7
wilsonhong <wilson100hong@gmail.com>
2018-12-01 22:34:04 -0800

    11205 AC

--- Volume_112/11205/led.cpp
+++ Volume_112/11205/led.cpp
@@ -1,18 +1,11 @@
+// 1. bitwise arithematics in building mask
+// 2. recursion
 #include <iostream>
 #include <vector>
 #include <set>
 using namespace std;
 typedef unsigned int UI;
 
-// number of zero
-int noz(UI x, int P) { 
-  int cnt = 0;
-  for (int i = 0; i < P; ++i) {
-    cnt += !(x & 1);
-    x = x>>1;
-  }
-  return cnt;
-}
 
 void dump_mask(UI x, int P) {
   for (int i = 0; i < P; ++i) {
@@ -22,45 +15,65 @@ void dump_mask(UI x, int P) {
   cout << endl;
 }
 
-bool has_dup(const vector<UI> numbers, UI mask, int P) {
-  set<UI> r;
+bool duplicate(const vector<UI> numbers, UI mask) {
+  set<UI> all;
   for (const UI& n : numbers) {
-    UI n_n = n & mask;
-    if (r.count(n_n)) {
+    UI masked = n & mask;
+    if (all.count(masked)) {
       return true;
     }
-    r.insert(n_n);
+    all.insert(masked);
   }
   return false;
 }
 
+/*
+int num_zeros(int P, UI mask) {
+    int nz = 0;
+    for (int i=0;i<P;++i) {
+        if (mask & 1 == 0)
+            nz++;
+        mask >> 1;
+    }
+    return nz;
+}
+*/
+
+int solve(const vector<UI>& numbers, int P, int index, UI mask, int nz) {
+    if (index == P) return nz;
+
+    // enable this bit
+    UI best = solve(numbers, P, index+1, mask, nz);
+    UI mask2 = mask & ( ((1<<P)-1) ^ (1 << index) );
+    // cout << "mask2: "; dump_mask(mask2, P);
+    if (!duplicate(numbers, mask2)) {
+        UI cand = solve(numbers, P, index+1, mask2, nz+1);
+        if (cand > best) {
+            best = cand;
+        }
+    }
+    return best;
+}
 
 int main() {
-  int TC;
-  cin >> TC;
-  while (TC-- > 0) {
+  int T;
+  cin >> T;
+  while (T-- > 0) {
     int P, N;
     cin >> P >> N;
     vector<UI> numbers;
     for (int n = 0; n < N; ++n) {
       UI x = 0;
       char c;
-      for (int p = 0; p < P; ++p) {
+      for (int i = 0; i < P; ++i) {
         cin >> c;
-        if (c == '1') {
-          x += (1 << p);
-        }
+        x = x << 1;
+        x += (c-'0');
       }
       numbers.push_back(x);
+      //dump_mask(x, P);
     }
 
-    int best = 0;
-    for (UI mask = (1 << P) - 1; mask > 0; --mask) {
-      int nz = noz(mask, P);
-      if (nz > best && !has_dup(numbers, mask, P)) {
-        best = nz;
-      }
-    }
-    cout << P - best << endl;
+    cout << P - solve(numbers, P, 0, (1<<P) - 1, 0) << endl;
   }
 }
